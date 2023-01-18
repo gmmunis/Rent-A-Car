4d2028631c3680791f5af88bec8aac260c62237b
gmmunis <gmmunis@hotmail.com>
2023-01-18 17:35:34 -0300

    specifications repository

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,4 +1,13 @@
+import { Specification } from "../model/Specification";
+
 interface ICreateSpecificationsDTO {
     name: string;
     description: string;
 }
+
+interface ISpecificationRepository {
+    create({ name, description }: ICreateSpecificationsDTO): void;
+    findByName(name: string): Specification;
+}
+
+export { ISpecificationRepository, ICreateSpecificationsDTO };
--- src/modules/cars/repositories/SpecificationsRepository.ts
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -1,3 +1,32 @@
-class SpecificationsRepository { }
+import { Specification } from "../model/Specification";
+import { ICreateSpecificationsDTO, ISpecificationRepository } from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationRepository {
+    private specifications: Specification[];
+
+    constructor() {
+        this.specifications = [];
+    }
+
+    create({ name, description }: ICreateSpecificationsDTO): void {
+        const specification = new Specification();
+
+        Object.assign(specification, {
+            name,
+            description,
+            created_at: new Date(),
+        });
+
+        this.specifications.push(specification);
+    }
+
+    findByName(name: string): Specification {
+        const specification = this.specifications.find(specification => specification.name === name);
+
+        return specification;
+    }
+
+
+}
 
 export { SpecificationsRepository };
